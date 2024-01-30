e15e4609688ec9fbdf49be3266e2b4ca9dee158b
Natalia Ramírez Benito <117905300+Natalia-Ramirez-Benito@users.noreply.github.com>
2024-01-30 10:51:58 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'HI1_T2_Interfaces',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
