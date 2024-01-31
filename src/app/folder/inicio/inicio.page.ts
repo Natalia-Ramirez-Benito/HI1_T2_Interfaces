8f4024b99d4362add499ed7ea21657e6eb2fc003
Natalia Ramírez Benito <117905300+Natalia-Ramirez-Benito@users.noreply.github.com>
2024-01-31 14:35:07 +0100

    3001

--- src/app/folder/inicio/inicio.page.ts
+++ src/app/folder/inicio/inicio.page.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.page.html',
   styleUrls: ['./inicio.page.scss'],
 })
-export class InicioPage implements OnInit {
+export class InicioPage  {
 
-  constructor() { }
+  constructor() {
 
-  ngOnInit() {
   }
 
 }
